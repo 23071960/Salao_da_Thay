570238f17de4b4c167bcca2b555a778cfa2b040c
Domicio Pedra <129445909+23071960@users.noreply.github.com>
2025-05-13 12:58:17 -0300

    Delete scripts directory

--- scripts/tema_escuro.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('toggle-theme').addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme');
-    this.textContent = document.body.classList.contains('dark-theme')
-      ? '☀️ Modo Claro'
-      : '🌙 Modo Escuro';
-  });
-  
